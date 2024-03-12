4fb31b5ccee46e7ab0727728a26d4321a41a2532
Joeri van Engelen <joerivanengelen@hotmail.com>
2024-03-12 15:22:47 +0100

    Improve plotting script: Force non-interactive backend and use imod.visualize.plot

--- scripts/5-plot.py
+++ scripts/5-plot.py
@@ -1,15 +1,32 @@
 import matplotlib.pyplot as plt
+
+plt.switch_backend("Agg")
+import contextily as ctx
+import imod
+import numpy as np
 import xarray as xr
 
 path_head_nc = snakemake.input["path_head_nc"]
 path_figure = snakemake.output["path_figure"]
 
-# %%
+# %% Open data
 heads = xr.open_dataarray(path_head_nc)
+
+# %% Select data to plot
+data_to_plot = heads.sel(layer=slice(3, 5)).mean(dim="layer").isel(time=-1)
 
 # %% Plot
 fig, ax = plt.subplots()
-heads.sel(layer=slice(3, 5)).mean(dim="layer").isel(time=3).plot(ax=ax)
+
+# Access background maps
+background_map = ctx.providers["OpenStreetMap"]["Mapnik"]
+
+colors = "viridis"
+levels = np.arange(-2.5, 18.5, 2.5)
+
+imod.visualize.plot_map(data_to_plot, colors, levels, basemap=background_map, fig=fig, ax=ax)
+
+ax.set_title("Groundwater heads (m +NAP)")
 
 # %% Write
 plt.savefig(path_figure)
